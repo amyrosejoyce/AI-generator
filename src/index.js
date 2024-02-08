4a97a7c5219dd0615244870a13af9ed78dce537b
amyrosejoyce <150541286+amyrosejoyce@users.noreply.github.com>
2024-02-08 00:41:14 +0000

    added event function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-let Search = document.querySelector("search-input");
+function showFact(event) {
+  alert("hello");
+}
+
+let searchField = document.querySelector("#search-input");
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", showFact);
