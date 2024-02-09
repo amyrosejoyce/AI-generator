371502b58580c73aadaaff1bbca9c883ba6ccc56
amyrosejoyce <150541286+amyrosejoyce@users.noreply.github.com>
2024-02-09 11:54:50 +0000

    added api functionality

--- src/index.js
+++ src/index.js
@@ -10,3 +10,10 @@ function showFact(event) {
 
 let factFormElement = document.querySelector("#fact-generator");
 factFormElement.addEventListener("submit", showFact);
+
+let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+let context =
+  "Your are a smart AI Assistant that tells interesting facts. The fact must be provided in HTML format. Example: <p>this is a fact</p>";
+let prompt = "Generate a short and interesting fact about dinosaurs. ";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("prompt");
